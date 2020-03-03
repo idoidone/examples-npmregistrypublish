b34dba8857c347fae9a4c017babd4ebdf997e30e
Nhan D Le <code.nhan@gmail.com>
2020-03-03 16:41:44 +0700

    More log

--- packages/somecli/cli.js
+++ packages/somecli/cli.js
@@ -2,3 +2,4 @@ console.log("This is log from `somecli`");
 console.log("This is second line");
 console.log("This is third line");
 console.log("This is fourth line");
+console.log("This is 5th line");
